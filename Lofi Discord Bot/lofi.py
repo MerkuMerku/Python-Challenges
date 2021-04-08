8f39762bcb9422f3ada221898c2b11859f8ceb3b
MerkuMerku <marjamdun@hotmail.co.uk>
2021-04-08 17:28:01 +0100

    chore: update python bot

--- Lofi Discord Bot/lofi.py
+++ Lofi Discord Bot/lofi.py
@@ -9,6 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+CHANNEL = os.getenv('DISCORD_CHANNEL')
 
 # set the client & command prefix
 client = commands.Bot(command_prefix="!")
@@ -27,13 +28,13 @@ async def on_ready():
         f'{guild.name} (id: {guild.id})'
     )
 
-lofi_init = ["Lofi Engaged!", "It's time to chill!", "Lofi Loading..."]
-session_init = ["Starting a session", "Good luck! Studying ", "Let's study"]
-session_end = ["Time's up! well done!",
-               "Let's take a break.", "Good job! You studied well"]
+lofiInit = ["Lofi Engaged!", "It's time to chill!", "Lofi Loading..."]
+sessionInit = ["Starting a session", "Good luck! Studying ", "Let's study"]
+sessionEnd = ["Time's up! well done!",
+              "Let's take a break.", "Good job! You studied well"]
 
 # strings for the help command
-helplist = ["```Commands\n1. !lofi       Play lofi hiphop - beats to relax/discord to\n2. !lofi dc    Disconnect the lofi bot\n3. !lofi study Study with lofi for a set number of minutes\n4. !lofi pom   Study to a set number of pommodoro intervals!```",
+helpList = ["```Commands\n1. !lofi       Play lofi hiphop - beats to relax/discord to\n2. !lofi dc    Disconnect the lofi bot\n3. !lofi study Study with lofi for a set number of minutes\n4. !lofi pom   Study to a set number of pommodoro intervals!```",
             "```!lofi Plays the lofi music bot indefinitely - ideal for chilling/studying for an unspecified amount of time.```",
             "```!lofi dc disconnects the lofi bot regardless of what it's doing at the time - chill vibes over.```",
             "```!lofi study [minutes] begins a lofi study session for the amount of time specified. The bot will join and play lofi, and you will be notified when the time limit is up. The bot will also disconnect.```",
@@ -42,16 +43,12 @@ helplist = ["```Commands\n1. !lofi       Play lofi hiphop - beats to relax/disco
 # function for checking if a string can be converted to an int
 
 
-def is_int(val):
+def isInt(val):
     try:
         num = int(val)
     except ValueError:
         return False
     return True
-
-
-def pommodoro(val)
-
 
 @client.command()
 async def lofi(ctx, arg1=None, arg2=None):
@@ -60,11 +57,11 @@ async def lofi(ctx, arg1=None, arg2=None):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
 
     if arg1 == None:
-        print("Lofi loading...")
-        await ctx.send(random.choice(lofi_init))
-        # set up voice client
+        await ctx.send(random.choice(lofiInit))
         await voiceChannel.connect()
+        print("test")
 
+    
     elif arg1 == "dc":
         if not voice.is_connected():
             return
@@ -73,19 +70,19 @@ async def lofi(ctx, arg1=None, arg2=None):
 
     elif arg1 == "help":
         if arg2 == None:
-            await ctx.send(helplist[0])
+            await ctx.send(helpList[0])
 
         elif arg2 == "!lofi":
-            await ctx.send(helplist[1])
+            await ctx.send(helpList[1])
 
         elif arg2 == "!lofi dc":
-            await ctx.send(helplist[2])
+            await ctx.send(helpList[2])
 
         elif arg2 == "!lofi study":
-            await ctx.send(helplist[3])
+            await ctx.send(helpList[3])
 
         elif arg2 == "!lofi pom":
-            await ctx.send(helplist[4])
+            await ctx.send(helpList[4])
 
         else:
             await ctx.send("Please choose one of the commands listed by the `!lofi help` command, and try again.")
@@ -96,17 +93,16 @@ async def lofi(ctx, arg1=None, arg2=None):
             await ctx.send("Please specify the amount of time you would like to study for\n e.g. `!lofi study 15`")
             return
 
-        elif is_int(arg2):
-            # Calculate the number of seconds to stay active
-            entered_time = round(int(arg2)*60)
+        elif isInt(arg2):
+            voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='General')
+            voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
 
-            # Alert the users that the session has begun for the selected time
-            await ctx.send(f"{random.choice(session_init)} for {arg2} minutes 🧠⚡")
+            #Connect and wait for the time period specified
+            await voiceChannel.connect()
+            time.sleep(int(arg2)*60)
 
-            # Sleep function for the set time
-            time.sleep(entered_time)
-            await ctx.send(f"Session ended - {random.choice(session_end)}")
-            # Disconnect the voice client if it's still there
+            # Alert the user and disconnect the voice client if it's still there
+            await ctx.send(f"Session ended")
             if voice.is_connected():
                 await voice.disconnect()
 
@@ -121,18 +117,32 @@ async def lofi(ctx, arg1=None, arg2=None):
             return
 
         elif is_int(arg2):
-            # Calculate the number of seconds to stay active
-            entered_time = round(int(arg2)*60)
+            i = 0
+            pommodoroCycle = [25, 5, 25, 5, 25, 5, 25, 15]
+            while i < arg2:
+                for x in pommodoroCycle:
+                    if (x+1) % 2 != 0:
+                        print("time to study for 25 minutes!")
+                    else:
+                        if x == 7:
+                            print("take a longer 15 minute break this time!")
+                        else:
+                            print("take a short 5 minute break.")
 
-            # Alert the users that the session has begun for the selected time
-            await ctx.send(f"{random.choice(session_init)} for {arg2} minutes")
+                    # run the timed connection function
+                    executionTime = pommodoroCycle[x]*60
+                    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='General')
+                    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
 
-            # Sleep function for the set time
-            time.sleep(entered_time)
-            await ctx.send(f"Session ended - {random.choice(session_end)}")
-            # Disconnect the voice client if it's still there
-            if voice.is_connected():
-                await voice.disconnect()
+                    # Connect and wait for the time period specified
+                    await voiceChannel.connect()
+                    time.sleep(executionTime)
+
+                    # Alert the user and disconnect the voice client if it's still there
+                    if voice.is_connected():
+                        await voice.disconnect()
+                    i += 1
+
         else:
             await ctx.send("Please enter a number, and try again")
             return
